9a39c8e0bb061e3fd56b5ed4e1bcf5146876e1bc
Óscar Manrique <oscar.manrique@vass.es>
2017-10-11 14:31:51 +0200

    Código descomentado

--- BlinkingLabel/Classes/AppInfo.swift
+++ BlinkingLabel/Classes/AppInfo.swift
@@ -1,36 +1,36 @@
-//
-//  AppInfo.swift
-//  ___PROJECTNAME___
-//
-//  Created by Oscar Manrique on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-//
 
-//import UIKit
-//
-//class AppInfo {
-//    
-//    
-//    static func getVersion() -> String {
-//        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
-//            return version
-//        }
-//        return ""
-//    }
-//    
-//    static func getBuild() -> String {
-//        if let version = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String {
-//            return version
-//        }
-//        return ""
-//    }
-//    
-//    static func bundleId() -> String{
-//        
-//        if let bundleId = Bundle.main.bundleIdentifier {
-//            return bundleId
-//        }
-//        return ""
-//    }
-//}
+  AppInfo.swift
+  ___PROJECTNAME___
+
+  Created by Oscar Manrique on ___DATE___.
+  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+
+
+import UIKit
+
+class AppInfo {
+    
+    
+    static func getVersion() -> String {
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            return version
+        }
+        return ""
+    }
+    
+    static func getBuild() -> String {
+        if let version = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String {
+            return version
+        }
+        return ""
+    }
+    
+    static func bundleId() -> String{
+        
+        if let bundleId = Bundle.main.bundleIdentifier {
+            return bundleId
+        }
+        return ""
+    }
+}
 
