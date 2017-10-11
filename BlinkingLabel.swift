9a6e2236e5c591d358b5f7766d02ef6c3f385c1d
Óscar Manrique <oscar.manrique@vass.es>
2017-10-11 14:09:33 +0200

    inclusión de la clase

--- BlinkingLabel.swift
+++ BlinkingLabel.swift
@@ -1,6 +1,6 @@
 //
 //  BlinkingLabel.swift
-//  BlinkingLabel
+//  Pods-BlinkingLabel_Example
 //
 //  Created by Óscar Manrique on 11/10/17.
 //
@@ -9,16 +9,18 @@ import UIKit
 
 class BlinkingLabel: UILabel {
 
-    public func startBlinking() {
-        let options : UIViewAnimationOptions = [.repeat, .autoreverse]
-        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
-            self.alpha = 0
-        }, completion: nil)
-    }
-    
-    public func stopBlinking() {
-        alpha = 1
-        layer.removeAllAnimations()
+    public class BlinkingLabel : UILabel {
+        public func startBlinking() {
+            let options : UIViewAnimationOptions = [.repeat, .autoreverse]
+            UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+                self.alpha = 0
+            }, completion: nil)
+        }
+        
+        public func stopBlinking() {
+            alpha = 1
+            layer.removeAllAnimations()
+        }
     }
 
 }
